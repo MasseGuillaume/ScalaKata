7963d850138c97f00e1ac4d35018624c961e0871
Guillaume Masse <masgui@gmail.com>
2013-07-01 23:29:37 -0400

    fix for locator statefull

--- src/main/scala/com/scalakata/boot/WebJars.scala
+++ src/main/scala/com/scalakata/boot/WebJars.scala
@@ -1,12 +1,14 @@
 package com.scalakata.boot
 
-import org.webjars.WebJarAssetLocator
+import org.webjars.{MultipleMatchesException, WebJarAssetLocator}
 
 import scala.util.Try
 
 import net.liftweb._
 import http._
 import util._
+import common._
+
 
 object WebJars {
   val locator = new WebJarAssetLocator()
@@ -14,14 +16,18 @@ object WebJars {
 
     ResourceServer.baseResourceLocation = "META-INF"
     ResourceServer.allow {
-      case _ if Try(locator.getFullPath(S.uri.drop("/webjars/".length))).isSuccess => true
+      case _ => true
     }
 
     LiftRules.statelessDispatch.prepend(NamedPF("Webjar service") {
       case r@Req(prefix :: tail, suffx, _) if (prefix == "webjars") => {
-        val test = Try(locator.getFullPath(S.uri.drop("/webjars/".length))).isSuccess
-        val ressource = locator.getFullPath(tail.mkString("/")).split("/").toList.drop(1)
-        ResourceServer.findResourceInClasspath(r, ressource)
+        try {
+          val path = locator.getFullPath(S.uri.drop("/webjars/".length)).split("/").drop(1).toList
+          ResourceServer.findResourceInClasspath(r, path)
+        } catch {
+          case _: IllegalArgumentException => () => Empty
+          case _: MultipleMatchesException => () => Empty
+        }
       }
     })
   }
