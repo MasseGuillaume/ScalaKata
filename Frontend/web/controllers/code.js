ceacacbfbcb33a45f0fac35834e44be14048c15f
Guillaume Massé <masgui@gmail.com>
2014-04-27 13:05:31 -0400

    edit codemirror options inside codemirror

--- Frontend/web/controllers/code.js
+++ Frontend/web/controllers/code.js
@@ -1,18 +1,49 @@
-app.controller('code', function code($scope, LANGUAGE){
-	$scope.code = "1+1";
+app.controller('code', function code($scope, $timeout, LANGUAGE){
+	var cm, 
+		code = "",
+		editing = false;
+
 	$scope.cmOptions = {
 		extraKeys: {"Ctrl-Space": "autocomplete"},
 		fixedGutter: true,
 		coverGutterNextToScrollbar: true,
 		lineNumbers: true,
-		mode: 'text/x-' + LANGUAGE,
 		theme: 'solarized dark',
 		smartIndent: false,
-		autofocus: true,
 		autoCloseBrackets: true,
 		styleActiveLine: true,
 		keyMap: "sublime",
 		highlightSelectionMatches: { showToken: false },
-		onLoad: function(cm_) { cm = cm_; }
+		onLoad: function(cm_) { 
+			cm = cm_;
+			cm.focus();
+		}
 	};
+
+	function setMode(edit){
+		if(edit) {
+			code = $scope.code;
+			$timeout(function(){
+				$scope.cmOptions.mode = 'application/json';
+				$scope.code = JSON.stringify($scope.cmOptions, null, '\t');
+			});
+		} else {
+			$timeout(function(){
+				$scope.code = code;
+				$scope.cmOptions.mode = 'text/x-' + LANGUAGE;
+			});
+		}
+	}
+	setMode(false);
+
+	$scope.toogleEdit = function(){
+		editing = !editing;
+		setMode(editing);
+	};
+	
+	$scope.$watch('code', function(){
+		if(editing) {
+			$scope.cmOptions = JSON.parse($scope.code);	
+		}
+	});
 });
