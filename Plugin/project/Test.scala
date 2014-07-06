abc8f782dd4d3e15191bf30a2a06a69de37f19f8
Guillaume Massé <masgui@gmail.com>
2014-07-06 13:42:01 -0400

    compile context for katas

--- Plugin/project/Test.scala
+++ Plugin/project/Test.scala
@@ -3,26 +3,27 @@ import Keys._
 
 object EvalBuild extends Build {
 
-	val kata = config("kata")
+	lazy val Kata = config("kata") extend (Compile)
 
 	lazy val kstart = taskKey[Unit]("Start scala kata")
 
 	lazy val test = Project(
 		id = "test",
 		base = file("."),
-		settings = Project.defaultSettings ++ Seq(
+		settings = Project.defaultSettings ++ 
+		inConfig(Kata)(Defaults.configSettings) ++ Seq(
 			offline := true,
 			fork in kstart := true,
 			connectInput in kstart := true,
-			ivyConfigurations += kata,
-			scalaVersion in kata := "2.11.2-SNAPSHOT",
+			ivyConfigurations += Kata,
+			scalaVersion in Kata := "2.11.2-SNAPSHOT",
 			libraryDependencies ++= Seq(
-				"org.scalamacros" % s"paradise_${(scalaVersion in kata).value}" % "2.1.0-SNAPSHOT" % "kata",
-				"org.scala-lang" % "scala-compiler" % (scalaVersion in kata).value % "kata"
-				//"org.scala-lang" % "scala-reflect" % (scalaVersion in kata).value ???
+				"org.scalamacros" % s"paradise_${(scalaVersion in Kata).value}" % "2.1.0-SNAPSHOT" % "kata",
+				"org.scala-lang" % "scala-compiler" % (scalaVersion in Kata).value % "kata"
+				//"org.scala-lang" % "scala-reflect" % (scalaVersion in Kata).value ???
 			),
-			scalaSource in kata := sourceDirectory.value / "kata",
-			dependencyClasspath in kata := update.value.select(configurationFilter("kata")) map(Attributed.blank),
+			scalaSource in Kata := sourceDirectory.value / "kata",
+			dependencyClasspath in Kata := update.value.select(configurationFilter("kata")) map(Attributed.blank),
 			kstart := {
 				start()
 			}
---
+++ Plugin/src/kata/Test.scala
@@ -0,0 +1,3 @@
+object Test {
+	val a = 1
+}
