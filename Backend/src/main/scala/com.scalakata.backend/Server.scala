217eb6b7a4b671001367aafe50c58b633d1f9fed
Guillaume Massé <masgui@gmail.com>
2014-08-05 11:06:37 -0400

    wait for server to be ready to open browser

--- Backend/src/main/scala/com.scalakata.backend/Server.scala
+++ Backend/src/main/scala/com.scalakata.backend/Server.scala
@@ -4,6 +4,7 @@ import com.scalakata.eval._
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
+import akka.util.Timeout
 import spray.can.Http
 
 object Boot {
@@ -16,10 +17,17 @@ object Boot {
 		val service = system.actorOf(Props(
 			classOf[ScalaKataActor], artifacts, codePrelude, code, scalacOptions
 		), "scalakata-service")
-		IO(Http) ! Http.Bind(service, host, port.toInt)
 
-		val ready = new java.net.Socket(host, readyPort.toInt)
-		ready.sendUrgentData(0)
-		ready.close()
+		import scala.concurrent.duration._
+		import akka.pattern.ask
+		implicit val bindingTimeout = Timeout(1.second)
+		import system.dispatcher
+		(IO(Http) ? Http.Bind(service, host, port.toInt)) onSuccess {
+			case _: Http.Bound => {
+				val ready = new java.net.Socket(host, readyPort.toInt)
+				ready.sendUrgentData(0)
+				ready.close()
+			}
+		}
 	}
 }
