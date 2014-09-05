1659cf5ffdae6f06887dfd1cfc9f00c8f5f56dae
Guillaume Massé <masgui@gmail.com>
2014-09-05 11:51:14 -0400

    my version of getUserMedia with audio/video object constaints + utf8 + line wrap for insight

--- Frontend/video/record.sh
+++ Frontend/video/record.sh
@@ -3,7 +3,7 @@ sleep 5 &&
 ffmpeg \
 -s 1366x768 -f x11grab -i :0.0 \
 -f alsa -i pulse \
--vcodec libx264 -b 5000k \
+-vcodec libx264 -b 3000k \
 -acodec aac -strict experimental \
 -af lowpass=f=5000 \
 -s hd720 -ab 320k -r 25 -g 25 -threads 0 output.mp4 &&
